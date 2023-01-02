7a244f653db01ea9765d7ace3ebd3b3e6d48df6a
TukaramShelar <tukaramshelar511@gmail.com>
2023-01-02 10:59:09 +0530

    done

--- multiplication.js
+++ multiplication.js
@@ -1,8 +1,9 @@
-function division()
+function multiply()
 {
 var a = document.getElementById("firstno").value
 var b = document.getElementById("secondno").value
 var result
-result  = a/b
-document.getElementById("result").value = result
+result = a*b
+ document.getElementById("result").value = result
+
 }
